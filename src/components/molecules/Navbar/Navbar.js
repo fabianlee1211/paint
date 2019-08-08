1c14f4ef26e6d02777be9a5665c0d4513a604e1b
fabianlee1211 <chleefabian@gmail.com>
2019-08-08 17:15:24 +0800

    feat: save as image

--- src/components/molecules/Navbar/Navbar.js
+++ src/components/molecules/Navbar/Navbar.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { useSelector } from 'react-redux'
 import './Navbar.css'
 
-const Navbar = ({ showNewModal, setShowNewModal }) => (
-  <nav className="Navbar">
-    <li className="Navbar__Item" onClick={() => setShowNewModal(!showNewModal)}>New</li>
-    <li className="Navbar__Item">Save</li>
-  </nav>
-)
+const Navbar = React.forwardRef(({ showNewModal, setShowNewModal }, canvasRef) => {
+  const downloadLink = useRef()
+  const { filename, size } = useSelector(state => state.canvas)
+  const saveAsImage = () => {
+    if (size.width > 0 && size.height > 0) {
+      downloadLink.current.download = `${filename}.png`
+      downloadLink.current.href = canvasRef.current.toDataURL('image/png')
+      downloadLink.current.click()
+    }
+  }
+  return (
+    <nav className="Navbar">
+      <li className="Navbar__Item" onClick={() => setShowNewModal(!showNewModal)}>New</li>
+      <li onClick={saveAsImage} className="Navbar__Item">Save</li>
+      {/* Invisible link for triggering save image */}
+      <a ref={downloadLink} />
+    </nav>
+  )
+})
 
 export default Navbar
