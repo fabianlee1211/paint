bae7954bace03cddef9344c2c2f013cea517564c
fabianlee1211 <chleefabian@gmail.com>
2019-08-08 15:36:23 +0800

    chore: update readme

--- src/components/organisms/Window/Window.js
+++ src/components/organisms/Window/Window.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Button from '../../atoms/Button/Button'
+import paintLogo from '../../../assets/Win95PaintLogo.png'
 import './Window.css'
 
-const Window = ({ closeOnly, children, title, onClick, ...props }) => (
+const Window = ({ closeOnly, children, title, onClick, showLogo, ...props }) => (
   <header {...props}>
     <div className="Window__Header">
-      <p className="Window__Title">{title}</p>
+      <div className="Window__TitleWrapper">
+        {showLogo && <img className="Window__Logo" src={paintLogo} />}
+        <p className="Window__Title">{title}</p>
+      </div>
       <div className="Window__Buttons">
         {!closeOnly && <Button>_</Button>}
         {!closeOnly && <Button>◻</Button>}
@@ -15,5 +19,9 @@ const Window = ({ closeOnly, children, title, onClick, ...props }) => (
     {children}
   </header>
 )
+
+Window.defaultProps = {
+  showLogo: false,
+}
 
 export default Window
